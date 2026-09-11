f5e6c5d67a4470fd0458839bfe963dd5e750dc12
Brandon <nathanaelbrandon11@gmail.com>
2026-09-11 13:14:37 +0700

    Add PulseScreen class and integrate Seconds timer functionality

--- app.py
+++ app.py
@@ -16,8 +16,19 @@ from seconds import Seconds
 
 name = "" 
 age =  0
+Window.clearcolor = (0.32, 0.54, 0.54, 0.19)
+btn_color = (0.98, 0.23, 0.8, 19)
 
 #------------SCREENS----------------#
+
+
+def check_int(str_num):
+    try:
+        return int(str_num)
+    except:
+        return False
+
+
 
 class InstructionScreen(Screen):
     def __init__(self, **kwargs):
@@ -60,11 +71,64 @@ class InstructionScreen(Screen):
             self.manager.current = 'pulse1'
 
 
+class PulseScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        instr = Label(text=txt_test1)
+        self.next_screen = False
+        self.lbl_sec = Seconds(15)
+        self.lbl_sec.bind(done=self.sec_finished)
+
+        line1 = BoxLayout(size_hint=(0.8, None), height='30sp')
+        lbl_result = Label(text='Enter the result:', halign='right')
+        self.in_result = TextInput(text='0', multiline=False)
+        self.in_result.set_disabled(True)
+
+        line1.add_widget(lbl_result)
+        line1.add_widget(self.in_result)
+
+        self.btn = Button(text='Next', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
+        self.btn.background_color = btn_color
+        self.btn.on_press = self.next
+        line2 = BoxLayout(size_hint=(0.8, None), height='30sp')
+
+        outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
+        outer.add_widget(instr)
+        outer.add_widget(self.lbl_sec)
+        outer.add_widget(line1)
+        outer.add_widget(self.btn)
+
+        self.add_widget(outer)
+
+    def sec_finished(self, *args):
+        self.next_screen = True
+        self.in_result.set_disabled(False)
+        self.btn.set_disabled(False)
+        self.btn.text = 'Next'
+
+
+    def next(self):
+        if not self.next_screen:
+            self.btn.set_disabled(True)
+            self.lbl_sec.start()
+        else:
+            global p1
+            p1 = check_int(self.in_result.text)
+            if p1 == False or p1 < 0:
+                p1 = 0
+                self.in_result.text = str(p1)
+            else:
+                self.manager.current = 'sits'
+                
+
+
+
 
 class MainApp(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(InstructionScreen(name='instruction'))
+        sm.add_widget(PulseScreen(name='pulse1'))
         return sm
 
 
